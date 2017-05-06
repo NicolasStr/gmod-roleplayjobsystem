9ceb09c47d391fea741f0f18418e07b182eabefc
Nicolas Str****** <nicolas.streng@gmail.com>
2017-05-06 21:23:43 +0200

    wow

---
+++ lua/rpjs/sh_config.lua
@@ -0,0 +1,21 @@
+rpjs.versdion = "1.8"
+
+rpjs.config = {}
+
+rpjs.config.lang = {}
+rpjs.config.windowtitle = 'JOBS'
+rpjs.config.windowbottomcolor = Color( 52, 73, 94, 255 )
+rpjs.config.windowtopcolor = Color( 44, 62, 80, 255 )
+rpjs.config.bgcolor = Color( 44, 62, 80, 255 )
+rpjs.config.titlecolor = Color( 236, 240, 241, 255 )
+rpjs.config.resumecolor = Color(231,76,60,255)
+rpjs.config.closebtncolor = Color(231,76,60,255)
+rpjs.config.barscolor = Color(231,76,60,255)
+rpjs.config.admins = {
+	"superadmin",
+	"admin"
+}
+rpjs.config.distance = 70  -- DISTANCE TO CHECK WHEN CHANGING JOB AND OPENING MENU
+
+-- SET THIS TO TRU IF YOU WANT TO ACTIVATE DEBUGGING COMMAND: rpjs_debug
+rpjs.config.debug = true
